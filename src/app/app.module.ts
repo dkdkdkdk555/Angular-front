2289c945cfff466d695395b78b8cbd112e7209b5
dkdkdkdk555 <dkdkdkdk555@naver.com>
2023-07-31 15:38:31 +0900

    모듈 생성 및 모듈화
    - 모듈생성
    - 모듈 import
    - 모듈 export

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { FooterComponent } from './footer/footer.component';
 import { SectionComponent } from './section/section.component';
 import { TimeDisplayComponent } from './section/time-display/time-display.component';
 import { ButtonsComponent } from './section/buttons/buttons.component';
+import { LayoutModule } from './layout/layout.module';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,13 @@ import { ButtonsComponent } from './section/buttons/buttons.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+    LayoutModule
+    
+  ],
+  exports:[
+    AppComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
