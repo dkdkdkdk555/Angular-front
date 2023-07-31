fad5d79f83f0df2d9e50f7175be53476f1dcba3b
dkdkdkdk555 <dkdkdkdk555@naver.com>
2023-07-31 19:24:22 +0900

    라우팅

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
+// 경로 설정 : 해당 url 에서 어떻게 동작하게 할지 설정
 const routes: Routes = [
   {
-  
+      path : '', // 루트 경로
+      redirectTo : 'stopwatch',
+      pathMatch : 'full'
   }
 ];
 
--- src/app/section/section.module.ts
+++ src/app/section/section.module.ts
@@ -4,19 +4,38 @@ import { SectionComponent } from './section.component';
 import { TimeDisplayComponent } from './stopwatch/time-display/time-display.component';
 import { ButtonsComponent } from './stopwatch/buttons/buttons.component';
 import { StopwatchModule } from './stopwatch/stopwatch.module';
+import { Route, RouterModule, Routes } from '@angular/router';
+import { StopwatchComponent } from './stopwatch/stopwatch.component';
+import { ClockComponent } from './clock/clock.component';
 
-
+const routes : Routes = [
+  {
+    path : 'stopwatch',
+    component: StopwatchComponent
+  },
+  {
+    path : 'clock',
+    component : ClockComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    SectionComponent
+    SectionComponent,
+    ClockComponent
   ],
   exports : [
-    SectionComponent
+    SectionComponent,
+    RouterModule // 라우터모듈이 설정값을 알게하기위해 exports
   ],
   imports: [
     CommonModule,
-    StopwatchModule
+    StopwatchModule,
+    RouterModule.forChild(routes) /* 루트에서 (app-routing.module.ts) 해주는게
+      아니기 때문에 forChild 에 routes 정보 넣는다.
+    */
+
+  
   ]
 })
 export class SectionModule { }
